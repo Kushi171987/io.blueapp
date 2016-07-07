df0fda55d53898ac4ca4ac6e0e149931e8f1fca5
Kushal Kumar <kushal@vensi.com>
2016-07-08 00:20:32 +0530

    Deleted old repo which is moved to blueapp org..

--- src/ios/BlueAppBridge.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDV.h>
-#import "AppDelegate.h"
-
-@interface BlueAppBridge : CDVPlugin
-
-- (void)init:(CDVInvokedUrlCommand*)command;
-- (void)message:(CDVInvokedUrlCommand*)command;
-
-@end
--- www/blueappbridge.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    request: function (action, message, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "BlueAppBridge", action, [message]);
-    }
-};
-
